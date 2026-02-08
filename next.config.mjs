aecf7a16dad6c2f140ecfe177909d4f682ae9a21
Yugank Singh <yuganksingh05@gmail.com>
2026-02-08 20:18:18 +0530

    Add swcMinify configuration based on NODE_ENV for production builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const shouldMinify = process.env.NODE_ENV === "production"
+
 const nextConfig = {
 	transpilePackages: ["dukon-core-lib"],
+	swcMinify: shouldMinify,
 }
 
 export default nextConfig
